f6ea08a08390f9c78780379a160e4e4e3f697a5f
Delta Student Ayush <akm22150809@gmail.com>
2026-06-10 00:02:56 +0530

    make schema and  add __init__.py and patient.py in api/vi folder and modify code in app __init__.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,1 +1,19 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
+from .api.v1.patient import router as patient_router
+
+def create_app():
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        app.state.db = await connect_mongo()
+        yield
+        await close_mongo()
+
+    app = FastAPI(lifespan=lifespan)
+
+    
+    app.include_router(patient_router,prefix="/patients")
+
+    return app
