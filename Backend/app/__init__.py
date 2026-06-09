eee3c46412a6bd94c551fba528901817b6a0a1fd
Abhishar Tripathi <ldcabhisharTripathi@gmail.com>
2026-06-09 20:13:33 +0530

    created app factory function and server runner file

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from .core.database import connect_mongo, close_mongo
+from contextlib import asynccontextmanager
 
+def create_app():
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        app.state.db = await connect_mongo()
+        yield
+        close_mongo()
+
+    app = FastAPI(lifespan=lifespan)
+
+    from .api.v1 import patient
+    app.include_router(patient.router)
+
+    return app
